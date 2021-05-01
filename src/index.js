5b664ae3a7cb7edfaca30544814dd9f3f7cef1a4
Fabian Streicher <fabi.streicher@gmail.com>
2021-05-01 13:39:42 +0200

    feat: setup boilerplate

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Hello World')
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('src/views'));
+
+// put client here
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/client/client.html'));
+});
+
+// put host dashboard here
+app.get('/host', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/host/host.html'));
+});
+
+
+app.listen(port, () => {
+  console.info(`listening on localhost:${port}`);
+});
