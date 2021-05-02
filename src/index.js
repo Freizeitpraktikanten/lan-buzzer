f092ddb7bc2c04f57b3b0bfef0ae7e40e2a3bd71
Ricardo Nußbaum <ricardo.nussbaum@posteo.de>
2021-05-02 19:57:00 +0200

    Copy Templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('Hello World')
+const path = require('path');
+const { clientHandler, hostHandler } = require('./socket');
+
+const express = require('express');
+const app = express();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer);
+
+
+const port = process.env.PORT || 3000;
+
+// serve static content
+app.use(express.static('src/views'));
+
+
+// put client here
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/client/client.html'));
+});
+
+// put host dashboard here
+app.get('/host', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/host/host.html'));
+});
+
+// socket communication
+const clientNamespace = io.of('/');
+const hostNamespace = io.of('/host');
+
+clientNamespace.on('connection', (socket) => {
+  clientHandler(socket, clientNamespace, hostNamespace);
+});
+
+hostNamespace.on('connection', (socket) => {
+  hostHandler(socket, hostNamespace, clientNamespace);
+});
+
+
+
+httpServer.listen(port, () => {
+  console.info(`listening on localhost:${port}`);
+});
---
+++ src/socket.js
@@ -0,0 +1,48 @@
+module.exports = {
+
+  /**
+   * Handle Client Communication
+   */
+  clientHandler(socket, clientNamespace, hostNamespace) {
+    console.info(`Client connected: ${socket.id}`);
+    hostNamespace.emit('clientConnect', socket.id);
+
+
+    socket.on('disconnect', (reason) => {
+      console.info(`Client disconnected: ${reason}`);
+      hostNamespace.emit('clientDisconnect', socket.id);
+    });
+
+    socket.on('buzz', (name, ack) => {
+      console.info('Client buzzed!', name, Date.now());
+      ack('Buzz received');
+    });
+
+    socket.onAny((event, ...args) => {
+      console.info(`received ${event}`);
+      console.info('/', clientNamespace.sockets.size);
+    });
+  },
+
+  /**
+   * Handle Host Communication
+   */
+  hostHandler(socket, hostNamespace, clientNamespace) {
+    console.info(`Host connected: ${socket.id}`);
+
+    socket.on('disconnect', (reason) => {
+      console.info(`Host disconnected: ${reason}`);
+    });
+
+    socket.on('queryClients', async cb => {
+      const clients = await clientNamespace.allSockets();
+      const ce = Array.from(clients.entries(), (v) => v.shift());
+      cb(ce);
+    });
+
+    socket.onAny((event, ...args) => {
+      console.info(`received ${event}`);
+    });
+  }
+
+};
