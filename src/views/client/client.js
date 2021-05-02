0e55c3b3a3f52521ef56a881d84c2f9ea3f82f80
Ricardo Nußbaum <ricardo.nussbaum@posteo.de>
2021-05-02 19:54:50 +0200

    Copy Templates

---
+++ src/views/client/client.js
@@ -0,0 +1,9 @@
+function buzz(socket) {
+  console.info('BUZZ!');
+  const tik = Date.now();
+  let tok = 0;
+  socket.emit('buzz', 'NAME', (ack) => {
+    tok = Date.now();
+    console.warn(ack, `${tok - tik}ms`);
+  });
+}
