2b88b2785933d0b90854690867ffdf8e5f6508b7
Ricardo Nußbaum <ricardo.nussbaum@posteo.de>
2021-05-03 21:17:32 +0200

    Welcome Page

--- src/views/client/client.js
+++ src/views/client/client.js
@@ -1,4 +1,14 @@
 function buzz(socket) {
+  sendBuzz(socket);
+  showRank();
+  window.navigator.vibrate(150); //Vibration - Only on Android
+}
+
+function showRank(){
+  document.getElementById("box").innerText = "Erfolgreich"
+}
+
+function sendBuzz(socket){
   console.info('BUZZ!');
   const tik = Date.now();
   let tok = 0;
@@ -6,10 +16,5 @@ function buzz(socket) {
     tok = Date.now();
     console.warn(ack, `${tok - tik}ms`);
   });
-  showRank();
-  window.navigator.vibrate(150); //Vibration - Only on Android
-}
 
-function showRank(){
-  document.getElementById("box").innerText = "Erfolgreich"
 }
