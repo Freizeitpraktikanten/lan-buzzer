0bcef945e2acb064c43a6993995fa1a906b12ab3
Fabian Streicher <fabi.streicher@gmail.com>
2021-09-05 13:33:32 +0200

    refactor(client): styles

--- src/views/client/client.js
+++ src/views/client/client.js
@@ -1,20 +1,29 @@
 //#region typedefs/const
 
+const BUZZ_MODE = document.querySelector('#buzz');
+const BUZZER_BUTTON = document.querySelector('#buzzer');
+const WELCOME_PAGE = document.querySelector('#welcome');
+const STATUS_BOX = document.querySelector('#box');
+const ANSWER_MODE = document.querySelector('#answerMode');
+const ANSWER_LABEL = document.querySelector('#answerModeLabel');
+const ANSWER_BOX = document.querySelector('#answer');
+const ANSWER_SUBMIT = document.querySelector('#submitAnswer');
+
 /**
- * 
+ * Game mode ENUM
+ * @readonly
+ * @enum {number}
  */
-const BUZZ_MODE = document.getElementById('buzz');
-const BUZZER_BUTTON = document.getElementById('buzzer');
-const WELCOME_PAGE = document.getElementById('welcome');
-const STATUS_BOX = document.getElementById('box');
-const ANSWER_MODE = document.getElementById('answerMode');
-const ANSWER_LABEL = document.getElementById('answerModeLabel');
-const ANSWER_BOX = document.getElementById('answer');
-const ANSWER_SUBMIT = document.getElementById('submitAnswer');
-const ROUND_MODE = {
+const GAME_MODE = {
   BUZZER: 0,
-  INPUT: 1
+  ANSWERS: 1
 };
+
+/**
+ * Player status ENUM
+ * @readonly
+ * @enum {number}
+ */
 const PLAYER_STATUS = {
   DISABLED: 0,
   ENABLED: 1
@@ -22,108 +31,33 @@ const PLAYER_STATUS = {
 
 //#endregion
 
-/* eslint-disable-next-line no-undef */
+/* global io */
 let socket = io('/');
 let playerStatus = PLAYER_STATUS.ENABLED;
-let roundMode = ROUND_MODE.BUZZER;
-let position = 0;
-let player = '';
+let gameMode = GAME_MODE.BUZZER;
+let position = null;
+let playerName = null;
+
+/**
+ * load player name from localstorage
+ */
+const nameFromStorage = localStorage.getItem('userName');
+if (nameFromStorage) {
+  document.querySelector('#playername').value = nameFromStorage;
+}
 
 socket.on('queryClients', () => {
-  if (player) { socket.emit('login', player, () => { }); }
+  if (playerName) { socket.emit('login', playerName); }
 });
 
 socket.on('updateClient', (payload) => {
-  console.info(payload.status + ' ' + payload.mode + ' ' + payload.position);
+  console.debug(payload);
   playerStatus = payload.status;
-  roundMode = payload.mode;
+  gameMode = payload.mode;
   position = payload.position;
   showRank();
   refresh();
 });
-
-function setName() {
-  player = document.getElementById('playerName').value;
-  console.info(player + ' logged in');
-  socket.emit('login', player, (ack) => {
-    console.warn(ack, player + ' logged in');
-  });
-  refresh();
-  showBuzzer();
-}
-function showBuzzer() {
-  WELCOME_PAGE.style.display = 'none';
-  BUZZ_MODE.style.visibility = 'visible';
-  ANSWER_MODE.style.visibility = 'hidden';
-}
-
-function showAnswerMode() {
-  WELCOME_PAGE.style.display = 'none';
-  BUZZ_MODE.style.visibility = 'hidden';
-  ANSWER_MODE.style.visibility = 'visible';
-}
-
-function buzz() {
-  if (playerStatus) {
-    sendBuzz();
-    if (navigator.vibrate) { navigator.vibrate(150); } //Vibration - Only on Android
-    refresh();
-  }
-}
-
-function showRank() {
-  STATUS_BOX.innerText = position ? position + '.' : 'Warten auf Host';
-}
-
-function submitAnswer() {
-  if (ANSWER_BOX.value.length > 0) {
-    sendAnswer();
-    //refresh();
-  } else {
-    ANSWER_LABEL.innerText = 'Keine leeren Antworten erlaubt';
-  }
-}
-function sendBuzz() {
-  console.info('BUZZ!');
-  socket.emit('buzz', (newPlayerStatus) => {
-    playerStatus = newPlayerStatus;
-  });
-}
-function sendAnswer() {
-  console.info('Submitted');
-  socket.emit('message', ANSWER_BOX.value);
-}
-
-function refresh() {
-  switch (playerStatus) {
-    case PLAYER_STATUS.ENABLED:
-      /*document.getElementById('buzzer').style.backgroundColor='red';*/
-      BUZZER_BUTTON.textContent = 'BUZZ';
-      BUZZER_BUTTON.disabled = false;
-      ANSWER_BOX.disabled = false;
-      ANSWER_SUBMIT.disabled = false;
-      ANSWER_LABEL.innerText = 'Bitte Antwort eingeben';
-      ANSWER_BOX.value = '';
-      STATUS_BOX.innerText = 'Let\'s buzz it';
-      break;
-    case PLAYER_STATUS.DISABLED:
-      /*document.getElementById('buzzer').style.backgroundColor='grey';*/
-      BUZZER_BUTTON.textContent = 'WAIT';
-      BUZZER_BUTTON.disabled = true;
-      ANSWER_BOX.disabled = true;
-      ANSWER_SUBMIT.disabled = true;
-      ANSWER_LABEL.innerText = 'Antwort gesendet.';
-      break;
-  }
-  switch (roundMode) {
-    case ROUND_MODE.BUZZER:
-      showBuzzer();
-      break;
-    case ROUND_MODE.INPUT:
-      showAnswerMode();
-      break;
-  }
-}
 
 /** 
  * catch any event for debugging purposes
@@ -131,3 +65,86 @@ function refresh() {
 socket.onAny((event, ...args) => {
   console.debug(`received event: ${event}\n`, ...args);
 });
+
+function setName() {
+  playerName = document.getElementById('playerName').value;
+  if (!playerName) {
+    alert('Please enter a name!');
+  } else {
+    localStorage.setItem('userName', playerName);
+    socket.emit('login', playerName);
+  }
+}
+
+function showBuzzer() {
+  WELCOME_PAGE.style.display = 'none';
+  BUZZ_MODE.style.display = 'flex';
+  ANSWER_MODE.style.display = 'none';
+}
+
+function showAnswerMode() {
+  WELCOME_PAGE.style.display = 'none';
+  BUZZ_MODE.style.display = 'none';
+  ANSWER_MODE.style.display = 'flex';
+}
+
+function buzz() {
+  if (playerStatus) {
+    // Vibration - only on Android
+    if (navigator.vibrate) { navigator.vibrate(150); }
+    sendBuzz();
+    refresh();
+  }
+}
+
+function showRank() {
+  STATUS_BOX.innerText = position ? `${position}.` : 'Warten auf Host';
+}
+
+function submitAnswer() {
+  const answer = ANSWER_BOX.value;
+  if (answer.length > 0) {
+    console.debug('Submitted', answer);
+    socket.emit('message', answer);
+    ANSWER_LABEL.innerText = 'Antwort gesendet';
+  } else {
+    ANSWER_LABEL.innerText = 'Keine leeren Antworten erlaubt';
+  }
+}
+
+function sendBuzz() {
+  console.debug('BUZZ!');
+  socket.emit('buzz', (newPlayerStatus) => {
+    playerStatus = newPlayerStatus;
+  });
+}
+
+function refresh() {
+  switch (playerStatus) {
+    case PLAYER_STATUS.ENABLED:
+      BUZZER_BUTTON.textContent = 'BUZZ';
+      BUZZER_BUTTON.disabled = false;
+      ANSWER_BOX.disabled = false;
+      ANSWER_SUBMIT.disabled = false;
+      ANSWER_LABEL.innerText = 'Bitte Antwort eingeben';
+      ANSWER_BOX.value = '';
+      STATUS_BOX.innerText = 'Let\'s buzz it!';
+      break;
+    case PLAYER_STATUS.DISABLED:
+      BUZZER_BUTTON.textContent = 'WAIT';
+      BUZZER_BUTTON.disabled = true;
+      ANSWER_BOX.disabled = true;
+      ANSWER_SUBMIT.disabled = true;
+      ANSWER_LABEL.innerText = 'Antwort gesendet';
+      break;
+  }
+
+  switch (gameMode) {
+    case GAME_MODE.BUZZER:
+      showBuzzer();
+      break;
+    case GAME_MODE.ANSWERS:
+      showAnswerMode();
+      break;
+  }
+}
